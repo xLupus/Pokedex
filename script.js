d2608bf85f404cd92b498cbe956a247898afb191
Vinicius <vinicius.am.souza221b@gmail.com>
2022-11-02 02:17:31 -0300

    XMLHttpRequest para Fetch

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-let getPokemon = new XMLHttpRequest();
 let lista      = document.getElementById('pokemons');
 let info       = document.getElementById('pokemon_details');
 
@@ -12,14 +11,12 @@ let card_color = {
     'Legend'  : '#a4c6bc'
 };
 
-getPokemon.open('GET', 'https://pokeapi.co/api/v2/pokemon?limit=151', true);
-getPokemon.send();
-getPokemon.onreadystatechange = function(){
-    if(getPokemon.readyState == 4 && getPokemon.status == 200) {
-        let response = JSON.parse(getPokemon.responseText);
-        response     = response.results
+fetch('https://pokeapi.co/api/v2/pokemon?limit=151')
+    .then( response => response.json())
+    .then( (json) => {
+        let pokemons = json.results;
 
-        for (let i = 0; i < response.length; i++) {
+        for (let i = 0; i < pokemons.length; i++) {
             let pokemon            = document.createElement('li');
             let pokemon_id         = document.createElement('span');
             let pokemon_nome       = document.createElement('span');
@@ -28,7 +25,7 @@ getPokemon.onreadystatechange = function(){
             pokemon_id.className   = 'pokemon_id';
             pokemon_nome.className = 'pokemon_name';
 
-            pokemon_nome.innerText = response[i].name.charAt(0).toUpperCase() + response[i].name.slice(1) ;
+            pokemon_nome.innerText = pokemons[i].name.charAt(0).toUpperCase() + pokemons[i].name.slice(1) ;
             pokemon_id.innerText   = (i < 9) ? '#0' + (i + 1) : '#' + (i + 1);
 
             pokemon.setAttribute('onclick', `getPokemonStats(${i + 1})`);
@@ -37,45 +34,49 @@ getPokemon.onreadystatechange = function(){
             pokemon.appendChild(pokemon_nome);
             lista.appendChild(pokemon);
         }
-    }
-}
+    })
+    .catch( (error) => {
+        console.log(error);
+    })
 
 function getPokemonStats(id){
-    getPokemon.open('GET', `https://pokeapi.co/api/v2/pokemon/${id}`, false);
-    getPokemon.send();
-    if( getPokemon.readyState == 4 && getPokemon.status == 200 ){
-        let response = JSON.parse(getPokemon.responseText);
-        console.log(response);
-        let pokemon_list    = document.getElementById('pokemon_info')
-        let pokemon_img     = document.getElementById('pokemon_img');
-        let pokemon_id      = document.getElementById('pokemon_info_id_value');
-        let pokemon_nome    = document.getElementById('pokemon_info_nome_value');
-        let pokemon_type    = document.getElementById('pokemon_info_type_value');
-        let pokemon_weight  = document.getElementById('pokemon_info_weight_value');
-        let pokemon_height  = document.getElementById('pokemon_info_height_value');
-        let base_experience = document.getElementById('pokemon_info_base-experience_value');
-        let pokemon_status  = document.getElementsByClassName('status');
-        let status_value    = document.getElementsByClassName('status_value');
-        let type            = [];
+    fetch(`https://pokeapi.co/api/v2/pokemon/${id}`)
+        .then( response => response.json())
+        .then( (json) => {
+            let pokemon_list    = document.getElementById('pokemon_info')
+            let pokemon_img     = document.getElementById('pokemon_img');
+            let pokemon_id      = document.getElementById('pokemon_info_id_value');
+            let pokemon_nome    = document.getElementById('pokemon_info_nome_value');
+            let pokemon_type    = document.getElementById('pokemon_info_type_value');
+            let pokemon_weight  = document.getElementById('pokemon_info_weight_value');
+            let pokemon_height  = document.getElementById('pokemon_info_height_value');
+            let base_experience = document.getElementById('pokemon_info_base-experience_value');
+            let pokemon_status  = document.getElementsByClassName('status');
+            let status_value    = document.getElementsByClassName('status_value');
+            let type            = [];
 
-        for(let i = 0; i < response.types.length; i++)
-            type[i] = response.types[i].type.name.charAt(0).toUpperCase() +  response.types[i].type.name.slice(1);
+            for(let i = 0; i < json.types.length; i++)
+                type[i] = json.types[i].type.name.charAt(0).toUpperCase() +  json.types[i].type.name.slice(1);
 
-        info.style.backgroundColor = card_color[type[0]];
-        
-        type = type.join(" / ");
+            info.style.backgroundColor = card_color[type[0]];
 
-        for(let i = 0; i < response.stats.length; i++){
-            status_value[i].innerText = response.stats[i].base_stat;
-            pokemon_status[i].appendChild(status_value[i]);
-        }
+            type = type.join(" / ");
 
-        pokemon_id.innerText      = (response.id < 10) ? pokemon_id.innerText = '0' + response.id : pokemon_id.innerText = response.id;
-        pokemon_img.src           = response.sprites.front_default;
-        pokemon_nome.innerText    = response.name.charAt(0).toUpperCase() + response.name.slice(1);
-        pokemon_type.innerText    = type;
-        pokemon_weight.innerText  = response.weight + 'kg';
-        pokemon_height.innerText  = response.height + '0cm';
-        base_experience.innerText = response.base_experience;
-    }
+            for(let i = 0; i < json.stats.length; i++){
+                status_value[i].innerText = json.stats[i].base_stat;
+                pokemon_status[i].appendChild(status_value[i]);
+            }
+
+            pokemon_id.innerText      = (json.id < 10) ? pokemon_id.innerText = '0' + json.id : pokemon_id.innerText = json.id;
+            pokemon_img.src           = json.sprites.front_default;
+            pokemon_nome.innerText    = json.name.charAt(0).toUpperCase() + json.name.slice(1);
+            pokemon_type.innerText    = type;
+            pokemon_weight.innerText  = json.weight + 'kg';
+            pokemon_height.innerText  = json.height + '0cm';
+            base_experience.innerText = json.base_experience;
+
+        })
+        .catch( (error) => {
+            console.log(error);
+        })
 }
